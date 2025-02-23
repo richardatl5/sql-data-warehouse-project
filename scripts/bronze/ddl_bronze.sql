cced40139797bfd047b37fd5154da6d653337c05
Richard Philipose <69731327+richardatl5@users.noreply.github.com>
2025-02-23 12:51:24 -0500

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,138 +1,62 @@
 /*
 =====================
-Bulk Insert
-Focus is on data completion and match from source, which currently is a local file.
-Tablock is used to lock the table during import.
+Creating Bronze level tables - focus is to directly capture all data from a source system
+Focus is on data completion and match from source
 
-Validated data count and mapping.
-
-Using truncate to delete the data, it is much faster to delete everything opposed to the delete function which takes longer to delete.
-Not using a where clause to delete.
 =====================
 */
 
-CREATE OR ALTER PROCEDURE bronze.load_bronze AS
-BEGIN
-DECLARE @start_time DATETIME, @end_time DATETIME
-	BEGIN TRY
-		--USE DataWarehouse;
-		--GO
 
-		-- CRM Cust Info
-		PRINT '==========================';
-		PRINT 'Loading Bronze Layer';
-		PRINT '==========================';
+CREATE TABLE bronze.crm_cust_info(
+	cst_id INT,
+	cst_key NVARCHAR(50),
+	cst_firstname NVARCHAR(50),
+	cst_lastname NVARCHAR(50),
+	cst_marital_status NVARCHAR(50),
+	cst_gender NVARCHAR(50),
+	cst_create_date DATE
 
+);
 
-		PRINT '--------------------------';
-		PRINT 'Loading CRM Tables';
-		PRINT '--------------------------';
-		SET @start_time = GETDATE();
+CREATE TABLE bronze.crm_sales_details (
+	sls_ord_num NVARCHAR(50),
+	sls_prd_key NVARCHAR(50),
+	sls_cust_id NVARCHAR(50),
+	sls_order_dt NVARCHAR(50),
+	sls_ship_dt NVARCHAR(50),
+	sls_due_dt NVARCHAR(50),
+	sls_sales INT,
+	sls_quantity INT,
+	sls_price INT
 
-		TRUNCATE TABLE bronze.crm_cust_info; 
+)
 
-			BULK INSERT bronze.crm_cust_info
+CREATE TABLE bronze.crm_prd_info (
+	prd_id INT,
+	prd_key NVARCHAR(50),
+	prd_nm NVARCHAR(50),
+	prd_cost INT,
+	prd_line  NVARCHAR(50),
+	prd_start_dt DATETIME,
+	prd_end_dt DATETIME
 
-			FROM 'C:\Users\phili\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+)
 
-			WITH (
-				FIRSTROW = 2,
-				FIELDTERMINATOR = ',',
-				TABLOCK
+CREATE TABLE bronze.erp_loc_a101 (
+	cid NVARCHAR(50),
+	cntry NVARCHAR(50)
+);
 
-			);
+CREATE TABLE bronze.erp_cust_az12 (
+	cid NVARCHAR(50),
+	bdate DATE,
+	gen NVARCHAR(50)
+);
 
-		-- CRM Prd Info
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+	id NVARCHAR(50),
+	cat NVARCHAR(50),
+	subcat NVARCHAR(50),
+	maintenance NVARCHAR(50),
+)
 
-		TRUNCATE TABLE bronze.crm_prd_info;
-
-			BULK INSERT bronze.crm_prd_info
-
-			FROM 'C:\Users\phili\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
-
-			WITH (
-				FIRSTROW = 2,
-				FIELDTERMINATOR = ',',
-				TABLOCK
-
-			);
-
-		-- CRM Sales_details
-
-		TRUNCATE TABLE bronze.crm_sales_details;
-
-			BULK INSERT bronze.crm_sales_details
-
-			FROM 'C:\Users\phili\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
-
-			WITH (
-				FIRSTROW = 2,
-				FIELDTERMINATOR = ',',
-				TABLOCK
-
-			);
-
-
-		PRINT '--------------------------';
-		PRINT 'Loading ERP Tables';
-		PRINT '--------------------------';
-
-		-- ERP CUST_AZ12
-
-		TRUNCATE TABLE bronze.erp_cust_az12;
-
-			BULK INSERT bronze.erp_cust_az12
-
-			FROM 'C:\Users\phili\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
-
-			WITH (
-				FIRSTROW = 2,
-				FIELDTERMINATOR = ',',
-				TABLOCK
-
-			);
-
-		-- ERP LOC_A101
-
-		TRUNCATE TABLE bronze.erp_loc_a101;
-
-			BULK INSERT bronze.erp_loc_a101
-
-			FROM 'C:\Users\phili\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
-
-			WITH (
-				FIRSTROW = 2,
-				FIELDTERMINATOR = ',',
-				TABLOCK
-			);
-
-		--ERP PX_CAT_G1V2
-
-		TRUNCATE TABLE bronze.erp_px_cat_g1v2;
-
-			BULK INSERT bronze.erp_px_cat_g1v2
-
-			FROM 'C:\Users\phili\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
-
-			WITH (
-				FIRSTROW = 2,
-				FIELDTERMINATOR = ',',
-				TABLOCK
-			);
-		
-		SET @end_time = GETDATE();
-	END TRY
-
-	BEGIN CATCH
-		PRINT'=====================';
-		PRINT'Loading Bronze Failed';
-		PRINT 'Error Message' + ERROR_MESSAGE();
-		PRINT'=====================';
-	END CATCH
-
-	PRINT'=====================';
-	PRINT 'Loading Time is ' +  CAST(DATEDIFF(ms,  @start_time, @end_time) AS NVARCHAR) + ' millseconds'  ;
-
-END
-	
---select * from bronze.erp_px_cat_g1v2
